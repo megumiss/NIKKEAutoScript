3c5fc143d8ebac58186fe2eb5f38921ac59c099f
megumiss <zlj12138@gmail.com>
2025-07-03 14:14:21 +0800

    format

--- module/solo_raid/solo_raid.py
+++ module/solo_raid/solo_raid.py
@@ -1,28 +1,29 @@
-from module.base.decorator import Config
 from module.base.timer import Timer
-from module.base.utils import crop
 from module.logger import logger
-from module.simulation_room.assets import AUTO_SHOOT, AUTO_BURST, END_FIGHTING
-from module.solo_raid.assets import *
-from module.ui.assets import GOTO_BACK, MAIN_CHECK
-from module.ui.ui import UI
 from module.ocr.ocr import Digit
+from module.simulation_room.assets import AUTO_BURST, AUTO_SHOOT, END_FIGHTING
+from module.solo_raid.assets import *
+from module.ui.assets import MAIN_CHECK
+from module.ui.ui import UI
+
 
 class NoOpportunityRemain(Exception):
     pass
 
+
 class SoloRaidIsUnavailable(Exception):
     pass
+
 
 class SoloRaid(UI):
     @property
     def free_remain(self) -> int:
         FREE_REMAIN = Digit(
             [FREE_OPPORTUNITY_CHECK.area],
-            name="FREE_REMAIN",
+            name='FREE_REMAIN',
             letter=(247, 247, 107),
             threshold=128,
-            lang="cnocr_num",
+            lang='cnocr_num',
         )
         return int(FREE_REMAIN.ocr(self.device.image))
 
@@ -30,11 +31,11 @@ class SoloRaid(UI):
     def free_opportunity_remain(self) -> bool:
         # result = self.appear(FREE_OPPORTUNITY_CHECK, offset=10, threshold=0.8)
         if self.free_remain:
-            logger.info(f"[Free opportunities remain] {self.free_remain}")
+            logger.info(f'[Free opportunities remain] {self.free_remain}')
         return self.free_remain
 
     def ensure_into_soloraid(self, skip_first_screenshot=True):
-        '''进入单人突击'''
+        """进入单人突击"""
         logger.hr('SOLO RAID START')
         click_timer = Timer(0.3)
         confirm_timer = Timer(3, count=3).start()
@@ -45,27 +46,29 @@ class SoloRaid(UI):
             else:
                 self.device.screenshot()
 
-            if click_timer.reached() \
-                    and self.appear(MAIN_CHECK, offset=10) \
-                    and self.appear_then_click(SOLO_RAID, offset=10, interval=2):
-                logger.info("Enter solo raid")
+            if (
+                click_timer.reached()
+                and self.appear(MAIN_CHECK, offset=10)
+                and self.appear_then_click(SOLO_RAID, offset=10, interval=2)
+            ):
+                logger.info('Enter solo raid')
                 continue
 
             if self.appear(SOLO_RAID_CHECK, offset=10):
                 break
 
             if confirm_timer.reached():
-                logger.error("Solo raid not found")
+                logger.error('Solo raid not found')
                 raise SoloRaidIsUnavailable
 
         if self.free_opportunity_remain:
             self.solo_raid()
         else:
-            logger.warning("There are no free opportunities")
+            logger.warning('There are no free opportunities')
             raise NoOpportunityRemain
 
     def solo_raid(self, skip_first_screenshot=True):
-        logger.hr("Start a solo raid")
+        logger.hr('Start a solo raid')
         click_timer = Timer(0.3)
 
         while 1:
@@ -75,70 +78,81 @@ class SoloRaid(UI):
                 self.device.screenshot()
 
             # 第七关扫荡
-            if click_timer.reached() \
-                    and self.appear(SOLO_RAID_CHECK, offset=10) \
-                    and self.appear(STAGE_SEVEN, offset=10) \
-                    and self.appear_then_click(CHALLENGE_QUICKLY_ENABLE, threshold=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(SOLO_RAID_CHECK, offset=10)
+                and self.appear(STAGE_SEVEN, offset=10)
+                and self.appear_then_click(CHALLENGE_QUICKLY_ENABLE, threshold=10, interval=1)
+            ):
                 click_timer.reset()
                 continue
 
             # 扫荡票max
-            if click_timer.reached() \
-                    and self.appear(CHALLENGE_QUICKLY_CHECK, offset=10) \
-                    and self.appear_then_click(CHALLENGE_QUICKLY_MAX, threshold=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(CHALLENGE_QUICKLY_CHECK, offset=10)
+                and self.appear_then_click(CHALLENGE_QUICKLY_MAX, threshold=10, interval=1)
+            ):
                 click_timer.reset()
                 continue
 
             # 扫荡确定
-            if click_timer.reached() \
-                    and self.appear(CHALLENGE_QUICKLY_CHECK, offset=10) \
-                    and not self.appear(CHALLENGE_QUICKLY_MAX, threshold=10) \
-                    and self.appear_then_click(CHALLENGE_QUICKLY_CONFIRM, offset=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(CHALLENGE_QUICKLY_CHECK, offset=10)
+                and not self.appear(CHALLENGE_QUICKLY_MAX, threshold=10)
+                and self.appear_then_click(CHALLENGE_QUICKLY_CONFIRM, offset=10, interval=1)
+            ):
                 click_timer.reset()
                 continue
 
             # 挑战
-            if click_timer.reached() \
-                    and self.appear(SOLO_RAID_CHECK, offset=10) \
-                    and self.appear_then_click(CHALLENGE_QUICKLY_DISABLE, threshold=10) \
-                    and self.appear_then_click(CHALLENGE, offset=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(SOLO_RAID_CHECK, offset=10)
+                and self.appear_then_click(CHALLENGE_QUICKLY_DISABLE, threshold=10)
+                and self.appear_then_click(CHALLENGE, offset=10, interval=1)
+            ):
                 click_timer.reset()
                 continue
 
             # 挑战确认
-            if click_timer.reached() \
-                    and self.appear(CHALLENGE_CONFIRM_CHECK, offset=10) \
-                    and self.appear_then_click(CHALLENGE_CONFIRM, offset=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(CHALLENGE_CONFIRM_CHECK, offset=10)
+                and self.appear_then_click(CHALLENGE_CONFIRM, offset=10, interval=1)
+            ):
                 click_timer.reset()
                 continue
 
             # 开始战斗
-            if click_timer.reached() \
-                    and self.appear(FIGHT_HISTORY, offset=10) \
-                    and self.appear_then_click(ENTER_FIGHT, offset=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(FIGHT_HISTORY, offset=10)
+                and self.appear_then_click(ENTER_FIGHT, offset=10, interval=1)
+            ):
                 click_timer.reset()
                 continue
 
-            if click_timer.reached() \
-                        and self.appear_then_click(AUTO_SHOOT, offset=10, interval=5, threshold=0.8):
-                    click_timer.reset()
-                    continue
+            if click_timer.reached() and self.appear_then_click(AUTO_SHOOT, offset=10, interval=5, threshold=0.8):
+                click_timer.reset()
+                continue
 
-            if click_timer.reached() \
-                    and self.appear_then_click(AUTO_BURST, offset=10, interval=5, threshold=0.8):
+            if click_timer.reached() and self.appear_then_click(AUTO_BURST, offset=10, interval=5, threshold=0.8):
                 click_timer.reset()
                 continue
 
             # 结束
-            if click_timer.reached() \
-                    and self.appear_then_click(END_FIGHTING, offset=10, interval=1):
+            if click_timer.reached() and self.appear_then_click(END_FIGHTING, offset=10, interval=1):
                 click_timer.reset()
                 break
 
             # 结算弹窗
-            if click_timer.reached() \
-                    and self.appear(ENEMY_DEFEATED, offset=10) \
-                    and self.appear_then_click(ENEMY_DEFEATED_CONFIRM, offset=10, interval=1):
+            if (
+                click_timer.reached()
+                and self.appear(ENEMY_DEFEATED, offset=10)
+                and self.appear_then_click(ENEMY_DEFEATED_CONFIRM, offset=10, interval=1)
+            ):
                 click_timer.reset()
                 break
 
@@ -157,7 +171,7 @@ class SoloRaid(UI):
             self.device.stuck_record_clear()
             return self.start_coop()
         else:
-            logger.info("There are no free opportunities")
+            logger.info('There are no free opportunities')
             raise NoOpportunityRemain
 
     def run(self):
