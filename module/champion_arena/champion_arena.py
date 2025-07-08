9049112d4b0868a4e04dc618f65b12482db10b3e
megumiss <zlj12138@gmail.com>
2025-07-09 01:19:45 +0800

    冠军竞技场

--- module/champion_arena/champion_arena.py
+++ module/champion_arena/champion_arena.py
@@ -1,11 +1,8 @@
+import random
 from functools import cached_property
 
 from module.base.arena import ArenaBase
 from module.base.timer import Timer
-from module.base.utils import (
-    _area_offset,
-    point2str,
-)
 from module.champion_arena.assets import *
 from module.logger import logger
 from module.ocr.ocr import Digit
@@ -19,90 +16,148 @@ class ChampionArenaIsUnavailable(Exception):
 
 
 class ChampionArena(UI, ArenaBase):
-    def start_competition(self, skip_first_screenshot=True):
-        logger.hr('Start a competition')
+    @cached_property
+    def cheers(self):
+        return [CHEER_SELECT_1, CHEER_SELECT_2]
 
+    @cached_property
+    def cheer_count(self) -> int:
+        """获取左侧应援数值"""
+        CHEER_LEFT_COUNT = Digit(
+            [CHEER_COUNT.area],
+            name='CHEER_LEFT_COUNT',
+            letter=(214, 105, 82),
+            threshold=128,
+            lang='cnocr_num',
+        )
+        return int(CHEER_LEFT_COUNT.ocr(self.device.image))
+
+    @property
+    def cheer_select(self) -> int:
+        """根据策略确定应援选择"""
+
+        if self.config.CheerStrategy_Strategy == 'Most':
+            return 0 if self.cheer_count > 50 else 1
+        elif self.config.CheerStrategy_Strategy == 'Few':
+            return 1 if self.cheer_count > 50 else 0
+        elif self.config.CheerStrategy_Strategy == 'Random':
+            return random.randint(0, 1)
+        else:
+            return 0
+
+    def cheer(self, skip_first_screenshot=True):
+        """应援"""
+        logger.info('Open promotion or champion')
         click_timer = Timer(0.3)
 
-        already_start = False
-
+        # 打开晋级赛或者冠军争霸赛
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
             else:
                 self.device.screenshot()
 
-            if not already_start and click_timer.reached() and self.free_opportunity_remain:
-                # 根据策略选择
-                opponent_id = 3
-                if self.config.OpponentSelection_Enable:
-                    opponent_id = self.select_strategy(True)['id']
-                opponent = self.button[opponent_id - 1]
-                logger.info(f'Secect opponent {opponent_id}')
-
-                self.device.click_minitouch(opponent[0], opponent[1])
-                logger.info('Click %s @ %s' % (point2str(opponent[0], opponent[1]), 'START_COMPETITION'))
+            # 晋级赛
+            if click_timer.reached() and self.appear_then_click(PROMOTION, offset=10, interval=3):
                 click_timer.reset()
                 continue
 
-            if click_timer.reached() and self.appear_then_click(SKIP, offset=(5, 5), interval=1):
+            # 冠军争霸赛
+            if click_timer.reached() and self.appear_then_click(CHAMPION, offset=10, interval=3):
                 click_timer.reset()
                 continue
 
+            # 应援结果
             if (
-                not already_start
-                and click_timer.reached()
-                and self.appear_then_click(INTO_COMPETITION, offset=(30, 30), interval=5, static=False)
+                click_timer.reached()
+                and self.appear(CHEER_RESULT, offset=10)
+                and self.appear_then_click(CHEER_RESULT_NEXT, offset=10, interval=1)
             ):
                 click_timer.reset()
                 continue
 
-            if click_timer.reached() and self.appear(END_COMPETITION, offset=5, interval=2):
-                logger.info('Click %s @ %s' % (point2str(100, 100), 'END_COMPETITION'))
-                self.device.handle_control_check(END_COMPETITION)
-                self.device.click_minitouch(100, 100)
-                already_start = True
+            # 检查应援按钮
+            if self.appear(PROMOTION_CHECK, offset=30) or self.appear(CHAMPION_CHECK, offset=30):
+                if self.appear(CHEER_ENABLE, offset=10):
+                    click_timer.reset()
+                    break
+                else:
+                    logger.info('Cheer already done')
+                    raise ChampionArenaIsUnavailable
+
+        logger.info('Cheer a nikke')
+        cheer_done = False
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if (
+                click_timer.reached()
+                and (self.appear(PROMOTION_CHECK, offset=30) or self.appear(CHAMPION_CHECK, offset=30))
+                and self.appear_then_click(CHEER_ENABLE, offset=10, interval=2)
+            ):
                 click_timer.reset()
                 continue
 
-            if already_start and self.appear(CHAMPION_ARENA_CHECK, offset=(10, 10), static=False):
-                break
+            # 应援选择
+            if (
+                click_timer.reached()
+                and self.appear(CHEER_CHECK, offset=10)
+                and self.appear_then_click(self.cheers[self.cheer_select], threshold=10, interval=1)
+            ):
+                click_timer.reset()
+                continue
 
-        if self.free_opportunity_remain:
-            self.device.click_record_clear()
-            self.device.stuck_record_clear()
-            return self.start_competition()
+            # 应援确认
+            if (
+                click_timer.reached()
+                and self.appear(CHEER_CHECK, offset=10)
+                and (not self.appear(self.cheers[0], threshold=10) or not self.appear(self.cheers[1], threshold=10))
+                and self.appear_then_click(CHEER_CONFIRM, threshold=10, interval=1)
+            ):
+                cheer_done = True
+                click_timer.reset()
+                continue
+
+            # 关闭
+            if (
+                cheer_done
+                and self.appear(CHEER_CHECK, offset=10)
+                and (not self.appear(self.cheers[0], threshold=10) or not self.appear(self.cheers[1], threshold=10))
+                and self.appear_then_click(CHEER_CANCEL, threshold=10, interval=1)
+            ):
+                logger.info('Cheer done')
+                break
 
     def ensure_into_champion_arena(self, skip_first_screenshot=True):
         logger.hr('CHAMPION ARENA START')
         click_timer = Timer(0.3)
 
+        # 进入竞技场
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
             else:
                 self.device.screenshot()
 
-            # TODO
-            if self.appear(NEXT_SEASON, offset=(50, 50)):
+            if self.appear(NEXT_SEASON, offset=10):
                 raise ChampionArenaIsUnavailable
 
-            if click_timer.reached() and self.appear_then_click(ARENA_GOTO_CHAMPION_ARENA, offset=(30, 30), interval=5):
+            if click_timer.reached() and self.appear_then_click(ARENA_GOTO_CHAMPION_ARENA, offset=30, interval=3):
                 click_timer.reset()
                 continue
 
-            if self.appear(CHAMPION_ARENA_CHECK, offset=(10, 10)):
+            if self.appear(CHAMPION_ARENA_CHECK, offset=30):
+                click_timer.reset()
                 break
-
-        if self.free_opportunity_remain:
-            self.start_competition()
-        else:
-            logger.info('There are no free opportunities')
 
     def run(self):
         self.ui_ensure(page_arena)
         try:
             self.ensure_into_champion_arena()
+            self.cheer()
         except ChampionArenaIsUnavailable:
             pass
         self.config.task_delay(server_update=True)
