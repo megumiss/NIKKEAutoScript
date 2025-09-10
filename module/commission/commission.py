33261d6a45d27ed4b1cffbb5dc28a700ad176de9
megumiss <zlj12138@gmail.com>
2025-09-11 01:01:39 +0800

    派遣优化

--- module/commission/commission.py
+++ module/commission/commission.py
@@ -6,42 +6,96 @@ from module.ui.page import page_commission
 from module.ui.ui import UI
 
 
+class NoOpportunity(Exception):
+    pass
+
+
 class Commission(UI):
-    def dispatch_and_claim(self, skip_first_screenshot=True):
-        logger.hr('Dispatch and claim commission')
-        confirm_timer = Timer(8, count=3).start()
+    def receive(self, skip_first_screenshot=True):
+        logger.hr('Receive commission', 1)
+        confirm_timer = Timer(2, count=3)
         click_timer = Timer(0.3)
+
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
             else:
                 self.device.screenshot()
 
+            # 没次数
+            if self.appear(COMMISSION_CHECK, offset=10) and self.appear(CLAIM_DONE, offset=10):
+                raise NoOpportunity
+
             if self.handle_reward(interval=1):
-                confirm_timer.reset()
                 click_timer.reset()
                 continue
 
-            if click_timer.reached() and (
-                self.appear_then_click(DISPATCH, offset=(30, 30), interval=2)
-                or self.appear_then_click(DISPATCH_CONFIRM, offset=(30, 30), interval=2)
+            # 全部领取
+            if click_timer.reached() and (self.appear_then_click(CLAIM, offset=10, interval=1)):
+                click_timer.reset()
+                continue
+
+            # 派遣弹窗和派遣按钮
+            if self.appear(COMMISSION_CHECK, offset=10) and self.appear(DISPATCH, threshold=10):
+                logger.info('Receive commission done')
+                break
+
+            # if self.appear(COMMISSION_CHECK, offset=10):
+            #     if not confirm_timer.started():
+            #         confirm_timer.start()
+            #     if confirm_timer.reached():
+            #         logger.info('Receive commission done')
+            #         break
+            # else:
+            #     confirm_timer.clear()
+
+    def select_favorite():
+        logger.hr('Select favorite item', 1)
+
+    def dispatch(self, skip_first_screenshot=True):
+        logger.hr('Dispatch commission', 1)
+        click_timer = Timer(0.3)
+
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            # 派遣
+            if (
+                click_timer.reached()
+                and self.appear(COMMISSION_CHECK, offset=10)
+                and self.appear_then_click(DISPATCH, offset=10, interval=1)
             ):
-                confirm_timer.reset()
                 click_timer.reset()
                 continue
 
-            if click_timer.reached() and (
-                self.appear_then_click(CLAIM, offset=(30, 30), interval=2)
-                or self.appear_then_click(DISPATCH_CONFIRM, offset=(30, 30), interval=2)
+            # 派遣确认
+            if (
+                click_timer.reached()
+                and self.appear(ITEM_LIST_CHECK, offset=10)  # 复用
+                and self.appear_then_click(DISPATCH_CONFIRM, offset=10, interval=1)
             ):
-                confirm_timer.reset()
                 click_timer.reset()
                 continue
 
-            if self.appear(COMMISSION_CHECK, offset=(10, 10)) and confirm_timer.reached():
+            if self.appear(COMMISSION_CHECK, offset=(10, 10)):
+                logger.info('Dispatch commission done', 1)
                 break
 
     def run(self):
+        logger.hr('Dispatch and claim commission')
         self.ui_ensure(page_commission)
-        self.dispatch_and_claim()
+        try:
+            # 领取
+            self.receive()
+            # 处理收藏品
+            if self.config.CollectionItems_Enable and not self.config.CollectionItems_NIKKE:
+                self.select_favorite()
+            # 派遣
+            self.dispatch()
+        except NoOpportunity:
+            logger.warning('Commission allready done')
+
         self.config.task_delay(server_update=True)
