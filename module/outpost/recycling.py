2913ef24e02f3085852606f80ce464962a65a2c9
megumiss <zlj12138@gmail.com>
2025-09-27 21:11:37 +0800

    循环室

--- module/outpost/recycling.py
+++ module/outpost/recycling.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta, timezone
 from functools import cached_property
 
+from module.base.button import merge_buttons
 from module.base.timer import Timer
 from module.logger import logger
 from module.outpost.assets import *
-from module.ui.assets import CLICK_TO_NEXT, COMMISSION_CHECK, SELECT_MAX
+from module.ui.assets import CLICK_TO_NEXT, SELECT_MAX
 from module.ui.page import page_recycling
 from module.ui.ui import UI
 
@@ -27,28 +28,77 @@ class Recycling(UI):
         logger.hr('Recycling special upgrade', 2)
         click_timer = Timer(0.3)
 
-        while 1:
-            self.device.screenshot()
+        # 获取所有level并去重
+        levels = TEMPLATE_RECYCLING_LEVEL.match_multi(self.device.image, threshold=0.65, name='RECYCLING_LEVEL')
+        levels = merge_buttons(levels, x_threshold=30, y_threshold=30)
 
-            # 全部领取
-            if click_timer.reached() and self.appear_then_click(CLAIM, threshold=20, interval=1):
-                click_timer.reset()
+        for level in levels:
+            if level.location[1] > 500 and level.location[1] < 550:
+                logger.info('Skip common upgrade')
                 continue
+            logger.hr('Recycling special upgrade', 3)
 
-            # 等待领取完毕
-            if self.appear(COMMISSION_CHECK, offset=10) and self.appear(CLAIM_DONE, threshold=20):
-                if not confirm_timer.started():
-                    confirm_timer.start()
-                if confirm_timer.reached():
-                    # 需要派遣
-                    if self.appear(DISPATCH, threshold=10):
-                        logger.info('Receive commission done')
-                        break
-                    # 没次数/进行中
-                    if self.appear(DISPATCH_DONE, threshold=10):
-                        raise NoOpportunity
-            else:
-                confirm_timer.clear()
+            # 打开某个企业/职业
+            while 1:
+                self.device.screenshot()
+
+                if click_timer.reached() and self.appear(RECYCLING_COMMON_UPGRADE, offset=30):
+                    self.device.click(level, click_offset=(0, 100))
+                    self.device.sleep(0.5)
+                    continue
+
+                if self.appear(RECYCLING_UPGRADE_CHECK, offset=30, static=False):
+                    logger.info('Open upgrade window')
+                    break
+
+            # 升级
+            auto_click = 0
+            while 1:
+                self.device.screenshot()
+
+                # 点击进行下一步
+                if click_timer.reached() and self.appear_then_click(CLICK_TO_NEXT, offset=30, interval=1, static=False):
+                    click_timer.reset()
+                    continue
+
+                # 自动选择
+                if (
+                    auto_click < 5
+                    and click_timer.reached()
+                    and self.appear(RECYCLING_UPGRADE_CHECK, offset=30, static=False)
+                    and self.appear_then_click(SYNCHRO_UPGRADE_AUTO, offset=30, static=False)
+                ):
+                    auto_click += 1
+                    click_timer.reset()
+                    continue
+
+                # 升级
+                if (
+                    auto_click > 4
+                    and click_timer.reached()
+                    and self.appear(RECYCLING_UPGRADE_CHECK, offset=30, static=False)
+                    and self.appear_then_click(RECYCLING_UPGRADE_CONFIRM, offset=30, interval=1)
+                ):
+                    auto_click = 0
+                    self.device.sleep(2)
+                    self.device.click_record_clear()
+                    click_timer.reset()
+                    continue
+
+                # 材料不够,关闭窗口
+                if (
+                    auto_click > 4
+                    and self.appear(RECYCLING_UPGRADE_CHECK, offset=30, static=False)
+                    and not self.appear(RECYCLING_UPGRADE_CONFIRM, threshold=10)
+                    and self.appear_then_click(RECYCLING_UPGRADE_CLOSE, offset=30, interval=1, static=False)
+                ):
+                    self.device.click_record_clear()
+                    click_timer.reset()
+                    continue
+
+                if self.appear(RECYCLING_COMMON_UPGRADE, offset=30):
+                    logger.info('Upgrade done')
+                    break
 
     def upgrade_common(self):
         logger.hr('Recycling common upgrade', 2)
@@ -59,7 +109,7 @@ class Recycling(UI):
             self.device.screenshot()
 
             # 通用研究
-            if click_timer.reached() and self.appear_then_click(SYNCHRO_COMMON_UPGRADE, offset=30, interval=1):
+            if click_timer.reached() and self.appear_then_click(RECYCLING_COMMON_UPGRADE, offset=30, interval=1):
                 click_timer.reset()
                 continue
 
@@ -82,7 +132,7 @@ class Recycling(UI):
             if (
                 max_click > 1
                 and self.appear(RECYCLING_UPGRADE_CHECK, offset=50)
-                and not self.appear(SYNCHRO_COMMON_UPGRADE_CONFIRM, threshold=10)
+                and not self.appear(RECYCLING_COMMON_UPGRADE_CONFIRM, threshold=10)
             ):
                 logger.info('Recycling common upgrade done')
                 break
@@ -95,13 +145,13 @@ class Recycling(UI):
             if (
                 click_timer.reached()
                 and self.appear(RECYCLING_UPGRADE_CHECK, offset=50)
-                and self.appear_then_click(SYNCHRO_UPGRADE_CLOSE, offset=30, interval=1, static=False)
+                and self.appear_then_click(RECYCLING_UPGRADE_CLOSE, offset=30, interval=1, static=False)
             ):
                 click_timer.reset()
                 continue
 
             # 返回
-            if click_timer.reached() and self.appear(SYNCHRO_COMMON_UPGRADE, offset=30):
+            if click_timer.reached() and self.appear(RECYCLING_COMMON_UPGRADE, offset=30):
                 logger.info('Back to recycling room')
                 break
 
