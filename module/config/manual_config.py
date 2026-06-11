3bfe05be2fe0823d49bcc392ace8a0efacdce63f
megumiss <zlj12138@gmail.com>
2026-06-11 22:12:59 +0800

    支持剧情活动ARK RANGER

--- module/config/manual_config.py
+++ module/config/manual_config.py
@@ -102,7 +102,7 @@ class ManualConfig:
             "event_type": 1,
             "mini_game": False,
             "mini_game_play": True,
-            "extend": True,
+            "extend": False,
             "story_part": "Story_1",  # Story_1, Story_2
             "story_difficulty": "Normal",  # Normal, Hard
             "pending_click_offset": (0, 0),
