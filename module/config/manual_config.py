2496530cd22bbb770c2f143838665671d9b2d559
megumiss <zlj12138@gmail.com>
2026-07-02 17:39:27 +0800

    支持剧情活动WAVE TO YOU

--- module/config/manual_config.py
+++ module/config/manual_config.py
@@ -96,6 +96,18 @@ class ManualConfig:
 
     EVENTS = [
         {
+            "event_id": "event_20260702",
+            "event_name": "WAVE TO YOU",
+            # 大型活动
+            "event_type": 1,
+            "mini_game": True,
+            "mini_game_play": True,
+            "extend": False,
+            "story_part": "Story_1",  # Story_1, Story_2
+            "story_difficulty": "Normal",  # Normal, Hard
+            "pending_click_offset": (0, 0),
+        },
+        {
             "event_id": "event_20260611",
             "event_name": "ARK RANGER",
             # 大型活动
