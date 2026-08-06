a17fb92329fb641a7253e9ef78e2478f041d518b
megumiss <zlj12138@gmail.com>
2026-08-06 19:55:30 +0800

    支持剧情活动PROJECT MATIS Story2 Hard

--- module/config/manual_config.py
+++ module/config/manual_config.py
@@ -104,7 +104,7 @@ class ManualConfig:
             "mini_game_play": True,
             "extend": False,
             "story_part": "Story_2",  # Story_1, Story_2
-            "story_difficulty": "Normal"  # Normal, Hard
+            "story_difficulty": "Hard"  # Normal, Hard
         },
         {
             "event_id": "event_20260702",
