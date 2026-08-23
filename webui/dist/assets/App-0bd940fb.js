0df7c193038c0655f8a31c7e9fb445af07227099
megumiss <zlj12138@gmail.com>
2026-08-23 10:48:46 +0800

    画面预览：进入互动模式时操作栏默认收起，切换按钮改为竖排三点图标

--- webui/dist/assets/App-0bd940fb.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./index-a19ef68d.js";import"./vue-2f12c2b1.js";export{m as default};
---
+++ webui/dist/assets/App-139871f2.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./index-66a0d482.js";import"./vue-2f12c2b1.js";export{m as default};
