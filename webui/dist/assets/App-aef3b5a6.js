0f238b013c06f8a0aa55aa149e28c2e8949a23f7
megumiss <zlj12138@gmail.com>
2026-09-03 10:21:57 +0800

    调整实例概览默认宽度

---
+++ webui/dist/assets/App-aef3b5a6.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./index-c20eda6a.js";import"./vue-b7be87a4.js";export{m as default};
--- webui/dist/assets/App-c22aaeda.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./index-94658fc3.js";import"./vue-b7be87a4.js";export{m as default};
